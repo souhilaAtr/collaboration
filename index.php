a01d4eac842fc3aebea49da698a2d9034582016f
Clement <clement.dimmi@hotmail.fr>
2019-04-29 14:22:41 +0200

    style.css

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
-echo " hello";
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">   
+    <title>Titre du site</title>
+    <link rel="stylesheet" href="asset/style.css">
+</head>
+<body>
+    <div class="wrap" id="superhero">
+        <div class="thor"> Thor </div>
+        <div class="ironman"> Iron Man </div>
+        <div class="hulk"> Hulk </div>
+        <div class="captainamerica"> Captain America </div>
+    </div>
+
+</body>
+</html>
